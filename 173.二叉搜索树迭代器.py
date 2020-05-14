9c7603759872194311bdcd94130252a8e0814370
lost-person <luzhangxd@outlook.com>
2020-05-14 10:52:45 +0800

    feat: 2020.05.12-2020.05.14 刷题记录

---
+++ 173.二叉搜索树迭代器.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode.cn id=173 lang=python3
+#
+# [173] 二叉搜索树迭代器
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.stack = []
+
+        self.__push_left_child(root)
+
+    def __push_left_child(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        top_node = self.stack.pop()
+
+        if top_node.right:
+            self.__push_left_child(top_node.right)
+        
+        return top_node.val
+
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return len(self.stack) > 0
+
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+# @lc code=end
+
---
+++ 174.地下城游戏.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=174 lang=python3
+#
+# [174] 地下城游戏
+#
+
+# @lc code=start
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        res = 0
+        if not dungeon or not dungeon[0]:
+            return res
+        
+        m, n = len(dungeon), len(dungeon[0])
+
+        dp = [[float('inf')] * (n + 1) for _ in range(m + 1)]
+        dp[m][n - 1] = dp[m - 1][n] = 1
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                dp[i][j] = max(1, min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j])
+
+        return dp[0][0]
+# @lc code=end
+
+
