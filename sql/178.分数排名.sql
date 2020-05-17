ea27499eb5e19cea6647ff98b90279cab3f92d7c
lost-person <luzhangxd@outlook.com>
2020-05-17 10:12:19 +0800

    feat: leetcode刷题之sql(非会员)

---
+++ sql/178.分数排名.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select s1.Score,count(distinct(s2.score)) 'Rank' 
+from Scores s1 join Scores s2 
+on s1.score<=s2.score 
+group by s1.Id 
+order by s1.Score desc;
