bd5c81dde0723d0e2f6c19bec46df47a34b4e4e6
lost-person <luzhangxd@outlook.com>
2020-05-21 09:08:32 +0800

    feat: 2020.05.21刷题记录

---
+++ 203.移除链表元素.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode.cn id=203 lang=python3
+#
+# [203] 移除链表元素
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if not head:
+            return head
+        
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        p, q = dummy, head
+        while q:
+            if q.val == val:
+                p.next = q.next
+            else:
+                p = q
+            q = q.next
+        return dummy.next
+# @lc code=end
+
