bdd3dc4ebed5af2cbe382607413d56a4278241cd
爱学习的程序员 <zhanglu@aixuexidechengxuyuandeMacBook-Pro.local>
2020-06-08 20:16:13 +0800

    feat: 2020.06.08刷题笔记

---
+++ 217.存在重复元素.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=217 lang=python3
+#
+# [217] 存在重复元素
+#
+
+# @lc code=start
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # 排序(略)
+        # hash表
+        if not nums or len(nums) < 2:
+            return False
+        
+        num_set = set(nums)
+        if len(num_set) == len(nums):
+            return False
+        else:
+            return True
+# @lc code=end
+
