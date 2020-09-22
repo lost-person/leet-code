e2d69b2b49d66bf8785e1fc12e5d9fddd81e3991
lost-person <luzhangxd@outlook.com>
2020-09-22 16:27:50 +0800

    增添快慢指针和保持相对顺序的解法

--- 面试题21. 调整数组顺序使奇数位于偶数前面.py
+++ 面试题21. 调整数组顺序使奇数位于偶数前面.py
@@ -16,3 +16,29 @@ class Solution:
             if low < high:
                 nums[low], nums[high] = nums[high], nums[low]
         return nums
+
+        # # 快慢指针
+        # slow, fast = 0, 0
+        # while fast < n:
+        #     if nums[fast] & 1:
+        #         nums[slow], nums[fast] = nums[fast], nums[slow]
+        #         slow += 1
+        #     fast += 1
+        # return nums
+
+        # # 相对顺序不变
+        # while i < n:
+        #     if nums[i] & 1 == 0:
+        #         j = i + 1
+        #         while j < n and nums[j] & 1:
+        #             j += 1
+                
+        #         if j == n: break
+
+        #         odd_num = nums[j]
+        #         while j > i:
+        #             nums[j] = nums[j - 1]
+        #             j -= 1
+        #         nums[j] = odd_num
+
+        #     i += 1
